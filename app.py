2956aaa4a43952866a38a7dfcde4f0c8d1544a1c
Alexander Bodian <polarzombies@gmail.com>
2018-06-02 17:51:59 -0400

    working example map
    got an example choropleth plotly map working inside dash

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import gdax
 import datetime
 import pytz
 import random
-
 from timezonefinder import TimezoneFinder
 import pytz
 from pytz import timezone, utc
@@ -20,40 +19,154 @@ from collections import deque
 
 mapbox_access_token = "pk.eyJ1IjoiamFja3AiLCJhIjoidGpzN0lXVSJ9.7YK6eRwUNFwd3ODZff6JvA"
 
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
+
+for col in df.columns:
+    df[col] = df[col].astype(str)
+
+scl = [[0.0, 'rgb(242,240,247)'],[0.2, 'rgb(218,218,235)'],[0.4, 'rgb(188,189,220)'],\
+            [0.6, 'rgb(158,154,200)'],[0.8, 'rgb(117,107,177)'],[1.0, 'rgb(84,39,143)']]
+
+df['text'] = df['state'] + '<br>' +\
+    'Beef '+df['beef']+' Dairy '+df['dairy']+'<br>'+\
+    'Fruits '+df['total fruits']+' Veggies ' + df['total veggies']+'<br>'+\
+    'Wheat '+df['wheat']+' Corn '+df['corn']
+
 app = dash.Dash()
 # https://goo.gl/f75Ufn
 # chrolopleth info
 
-app.layout = html.Div(children=[
+
+app.layout = html.Div([
+    dcc.Graph(
+        id = 'graph',
+        figure = {
+            'data' : [
+                go.Choropleth(
+                    colorscale = scl,
+                    autocolorscale = False,
+                    locations = df['code'],
+                    z = df['total exports'].astype(float),
+                    locationmode='USA-states',
+                    text=df['text'],
+                    marker=dict(
+                        line=dict(
+                            color='rgb(255,255,255)',
+                            width=2
+                        )),
+                    colorbar=dict(
+                        title="Millions USD")
+                )],
+            'layout': go.Layout(
+                title = '2011 US Agriculture Exports by State<br>(Hover for breakdown)',
+                geo = dict(
+                    scope = 'usa',
+                    projection = dict (type= 'albers usa'),
+                    showlakes = True,
+                    lakecolor = 'rgb(255,255,255)',
+                )
 
 
 
-    html.Div(
+            )
 
-        children=dcc.Graph(
-            id='graph',
-            figure={
-                'data': [{
-                    'lat': 30, 'lon': 30, 'type': 'choropleth', 'locationmode': 'USA-states',
-                }],
-                'layout': {
-                    'geo': {
-                        'scope': (
-                            'usa'
-                        )
-                    },
-                    'margin': {
-                        'l': 0, 'r': 0, 'b': 0, 't': 0
-                    },
-                }
-            }
-        )
+
+
+
+        }
     )
 ])
-    
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# app.layout = html.Div(children=[
+#
+#
+#
+#     html.Div(
+#
+#         children=dcc.Graph(
+#             id='graph',
+#             figure={
+#                 'data': [{
+#                     'lat': 30, 'lon': 30, 'type': 'choropleth', 'locationmode': 'USA-states',
+#                 }],
+#                 'layout': {
+#                     'geo': {
+#                         'scope': (
+#                             'usa'
+#                         )
+#                     },
+#                     'margin': {
+#                         'l': 0, 'r': 0, 'b': 0, 't': 0
+#                     },
+#                 }
+#             }
+#         )
+#     )
+# ])
+
+# import plotly.plotly as py
+# import pandas as pd
+#
+# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
+#
+# for col in df.columns:
+#     df[col] = df[col].astype(str)
+#
+# scl = [[0.0, 'rgb(242,240,247)'], [0.2, 'rgb(218,218,235)'], [0.4, 'rgb(188,189,220)'], \
+#        [0.6, 'rgb(158,154,200)'], [0.8, 'rgb(117,107,177)'], [1.0, 'rgb(84,39,143)']]
+#
+# df['text'] = df['state'] + '<br>' + \
+#              'Beef ' + df['beef'] + ' Dairy ' + df['dairy'] + '<br>' + \
+#              'Fruits ' + df['total fruits'] + ' Veggies ' + df['total veggies'] + '<br>' + \
+#              'Wheat ' + df['wheat'] + ' Corn ' + df['corn']
+#
+# data = [dict(
+#     type='choropleth',
+#     colorscale=scl,
+#     autocolorscale=False,
+#     locations=df['code'],
+#     z=df['total exports'].astype(float),
+#     locationmode='USA-states',
+#     text=df['text'],
+#     marker=dict(
+#         line=dict(
+#             color='rgb(255,255,255)',
+#             width=2
+#         )),
+#     colorbar=dict(
+#         title="Millions USD")
+# )]
+#
+# layout = dict(
+#     title='2011 US Agriculture Exports by State<br>(Hover for breakdown)',
+#     geo=dict(
+#         scope='usa',
+#         projection=dict(type='albers usa'),
+#         showlakes=True,
+#         lakecolor='rgb(255, 255, 255)'),
+# )
+#
+# fig = dict(data=data, layout=layout)
+# py.iplot(fig, filename='d3-cloropleth-map')
 
 
 
