8e0ef7e2141b37c7fa762e9cb796e3ca4c6d4dfd
Alexander Bodian <polarzombies@gmail.com>
2018-06-17 19:49:32 -0400

    Starting working on background checks data
    Starting working on background checks data

---
+++ background-checks/background.py
@@ -0,0 +1,85 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import gdax
+import plotly
+from dash.dependencies import Input, Output, Event
+import gdax
+import datetime
+import pytz
+import random
+from timezonefinder import TimezoneFinder
+import pytz
+from pytz import timezone, utc
+from pytz.exceptions import UnknownTimeZoneError
+import pandas as pd
+import plotly
+import plotly.graph_objs as go
+from collections import deque
+import json
+mapbox_access_token = "pk.eyJ1IjoiamFja3AiLCJhIjoidGpzN0lXVSJ9.7YK6eRwUNFwd3ODZff6JvA"
+
+state_to_code = {
+    # # Other
+    # 'District of Columbia': 'DC',
+    
+    # States
+    'Alabama': 'AL',
+    'Montana': 'MT',
+    'Alaska': 'AK',
+    'Nebraska': 'NE',
+    'Arizona': 'AZ',
+    'Nevada': 'NV',
+    'Arkansas': 'AR',
+    'New Hampshire': 'NH',
+    'California': 'CA',
+    'New Jersey': 'NJ',
+    'Colorado': 'CO',
+    'New Mexico': 'NM',
+    'Connecticut': 'CT',
+    'New York': 'NY',
+    'Delaware': 'DE',
+    'North Carolina': 'NC',
+    'Florida': 'FL',
+    'North Dakota': 'ND',
+    'Georgia': 'GA',
+    'Ohio': 'OH',
+    'Hawaii': 'HI',
+    'Oklahoma': 'OK',
+    'Idaho': 'ID',
+    'Oregon': 'OR',
+    'Illinois': 'IL',
+    'Pennsylvania': 'PA',
+    'Indiana': 'IN',
+    'Rhode Island': 'RI',
+    'Iowa': 'IA',
+    'South Carolina': 'SC',
+    'Kansas': 'KS',
+    'South Dakota': 'SD',
+    'Kentucky': 'KY',
+    'Tennessee': 'TN',
+    'Louisiana': 'LA',
+    'Texas': 'TX',
+    'Maine': 'ME',
+    'Utah': 'UT',
+    'Maryland': 'MD',
+    'Vermont': 'VT',
+    'Massachusetts': 'MA',
+    'Virginia': 'VA',
+    'Michigan': 'MI',
+    'Washington': 'WA',
+    'Minnesota': 'MN',
+    'West Virginia': 'WV',
+    'Mississippi': 'MS',
+    'Wisconsin': 'WI',
+    'Missouri': 'MO',
+    'Wyoming': 'WY',
+}
+
+
+code_to_state = {v: k for k, v in state_to_code.items()}
+
+
+df = pd.read_csv('nics-firearm-background-checks.csv')
+
+print(df)
