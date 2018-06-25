2af377b0cd11da1f395b728726b8e767d5f03df1
alexbodian <polarzombies@gmail.com>
2018-06-25 17:39:44 -0400

    Added range slider
    Added a range slider so you can control the range of years that will appear on the map of US mass shootings

--- shootings/map.py
+++ shootings/map.py
@@ -40,6 +40,7 @@ df = pd.read_csv('https://docs.google.com/spreadsheets/d/1b9o6uDO18sLxBqPwl_Gh9b
 desc = []
 lat = []
 lon = []
+year = []
 
 
 for i in range(0, df.shape[0]):
@@ -54,50 +55,149 @@ for i in range(0, df.shape[0]):
 
     lat.append(df.loc[i, 'latitude'])
     lon.append(df.loc[i, 'longitude'])
+    year.append(df.loc[i,'year'])
+
+
+# print(year)
+uniq_years = df['year'].unique()
+uniq_years = uniq_years[::-1]
+# print(type(uniq_years))
+# uniq_years = uniq_years.iloc[::-1]
+year_dict = {}
+for i in uniq_years:
+    year_dict[str(i)] = i
+
+
+
 
 
 app = dash.Dash()
 
-app.layout = html.Div([dcc.Graph(id='shooting_locations',
-                    figure = {'data': [
-                        go.Scattermapbox(
-                            lat=lat,
-                            lon=lon,
-                            mode='markers',
-                            marker = {
-                                'size' :14,
-                                },
-                            text=desc,
-                        )],
-                    'layout': go.Layout(
-                            title = 'US Mass Shooting Locations', 
-                            width = 1200,
-                            height = 800,
-                            autosize=True,
-                            hovermode='closest',
-                            mapbox=dict(
-                                accesstoken=mapbox_access_token,
-                                bearing=0,
-                                center=dict(
-                                    lat=39.5,
-                                    lon=-98.35
-                                            ),
-                                pitch=0,
-                                zoom=2
+app.layout = html.Div([
+    
+    html.Div([
+    
+    dcc.Graph(id='shooting_locations'),
+
+    dcc.RangeSlider(
+        id='shooting_range',
+        min=1982,
+        max=2018,
+        step=None,
+        marks=year_dict,
+        value=[1982,2018]
+    )
+
+    ],style={'width':'80%', 'paddingLeft': 35})
+
+])
+
+@app.callback(Output('shooting_locations', 'figure'),
+             [Input('shooting_range','value')])
+def update_figure(selected_years):
+    # selected_years is a list
+
+
+    # desc = []
+    # lat = []
+    # lon = []
+    # year = []
+
+    desc_temp = []
+    lat_temp = []
+    lon_temp = []
+    year_temp = []
+
+    count = 0
+
+    for i in year:
+    
+        if (i >= selected_years[0]) and (i <= selected_years[1]):
+            desc_temp.append(desc[count])
+            lat_temp.append(lat[count])
+            lon_temp.append(lon[count])
+            year_temp.append(year[count])
+        count += 1
+
+
+    return {
+        'data': [
+            go.Scattermapbox(
+                lat=lat_temp,
+                lon=lon_temp,
+                mode='markers',
+                marker = {
+                    'size' :14,
+                    },
+                text=desc_temp,
+            )],
+        'layout': go.Layout(
+                title = 'US Mass Shooting Locations', 
+                width = 1200,
+                height = 800,
+                autosize=True,
+                hovermode='closest',
+                mapbox=dict(
+                    accesstoken=mapbox_access_token,
+                    bearing=0,
+                    center=dict(
+                        lat=39.5,
+                        lon=-98.35
                                 ),
-                                )
-                    
-                    
-                    
-                    }
+                    pitch=0,
+                    zoom=2
+                    ),
+                    )
 
 
 
+}
 
-)])
+
 
 if __name__ == '__main__':
     app.run_server()
+
+
+
+
+
+
+                    # figure = {'data': [
+                    #     go.Scattermapbox(
+                    #         lat=lat,
+                    #         lon=lon,
+                    #         mode='markers',
+                    #         marker = {
+                    #             'size' :14,
+                    #             },
+                    #         text=desc,
+                    #     )],
+                    # 'layout': go.Layout(
+                    #         title = 'US Mass Shooting Locations', 
+                    #         width = 1200,
+                    #         height = 800,
+                    #         autosize=True,
+                    #         hovermode='closest',
+                    #         mapbox=dict(
+                    #             accesstoken=mapbox_access_token,
+                    #             bearing=0,
+                    #             center=dict(
+                    #                 lat=39.5,
+                    #                 lon=-98.35
+                    #                         ),
+                    #             pitch=0,
+                    #             zoom=2
+                    #             ),
+                    #             )
+                    
+                    
+                    
+                    # }
+
+
+
+
 
 
 
